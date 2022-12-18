c7f955e042216bea271a811672df4074a61d9261
hingobway <hingobway@users.noreply.github.com>
2022-12-18 02:49:09 -0500

    cf deploy test

---
+++ pages/hello.js
@@ -0,0 +1,14 @@
+import Head from 'next/head';
+
+const hello = () => {
+  return (
+    <>
+      <Head>
+        <title>hello cloudflare</title>
+      </Head>
+      <div>Hello Cloudflare!</div>
+    </>
+  );
+};
+
+export default hello;
