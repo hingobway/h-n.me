c567974ee3d3af54bedd6ab883888b3e67f8b46d
hingobway <hingobway@users.noreply.github.com>
2024-09-18 01:55:43 -0400

    fix 6-digit gen

--- apps/server/src/generate.js
+++ apps/server/src/generate.js
@@ -8,6 +8,13 @@ module.exports.generateKey = async (len) =>
     .replace(/\+/g, '-')
     .replace(/\//g, '_');
 
-module.exports.generate6Digit = async () =>
-  // prettier-ignore
-  ('' + parseInt((await promisify(crypto.randomBytes)(3)).toString('hex'), '16')).slice(0,6);
+export const generateNumerical = async (length) =>
+  (
+    Array(length).fill(0).join('') +
+    parseInt(
+      (await promisify(randomBytes)(Math.ceil(length / 2))).toString('hex'),
+      16
+    )
+  ).slice(0 - length);
+
+export const generate6Digit = () => generateNumerical(6);
